45c292ed7fc7da6d26b89e49ef795f26edb0f77d
Jishinix <jishinix@gmail.com>
2025-09-18 18:26:36 +0200

    refactor: DiscordBot.ts kiss

--- src/DiscordBot.ts
+++ src/DiscordBot.ts
@@ -1,11 +1,12 @@
 import { EventDispatcher } from "./EventDispatcher.js";
-import { Partials, REST, Routes, ActivityType, Client, IntentsBitField, GatewayIntentBits, Guild, SlashCommandBuilder, InteractionType, Interaction, CDN } from "discord.js";
+import { Partials, REST, Routes, ActivityType, Client, IntentsBitField, GatewayIntentBits, Guild, SlashCommandBuilder, InteractionType, Interaction, CDN, AutocompleteInteraction, ChatInputCommandInteraction } from "discord.js";
 import fs from "fs";
 import { BotUtils } from "./BotUtils.js";
 import { DefaultEmbeds } from './DefaultEmbeds.js'
 import { PageModulManager } from './PageModulManager.js';
 import createDB from './DB.js';
 import { homePath } from "./dirname.js";
+import path from "path";
 
 const eventNames = [
     'guildBanAdd',
@@ -108,7 +109,7 @@ export class DiscordBot extends EventDispatcher {
         this.defaultEmbeds = new DefaultEmbeds(this);
         this.pageModulManager = new PageModulManager(this);
 
-        this.commands = new Map();
+        this.commands = new Map();  // Plugins can register commands in the PluginConstructor
 
         this.plugins = {};
 
@@ -127,31 +128,35 @@ export class DiscordBot extends EventDispatcher {
         const plugins = Object.keys(this.settings.plugins);
 
         for (let i = 0; i < plugins.length; i++) {
-            const pluginFolderPath = `${homePath}/plugins/${plugins[i]}`;
-            if (fs.existsSync(pluginFolderPath)) {
-                const pluginPath = `${pluginFolderPath}/${plugins[i]}.js`;
-                if (fs.existsSync(pluginPath)) {
-                    const pluginName = plugins[i].slice(0, 1).toLowerCase() + plugins[i].slice(1, plugins[i].length);
-                    const moduleClass = (await import(pluginPath)).default;
-                    if (moduleClass) {
-                        if ('preloadData' in moduleClass) {
-                            const preloadData = await moduleClass.preloadData(this);
-                            this.plugins[pluginName] = new moduleClass(this, preloadData);
-                        } else {
-                            this.plugins[pluginName] = new moduleClass(this);
-                        }
+            const pluginPath = path.join(homePath, 'plugins', plugins[i], plugins[i]);
+            const pluginName = plugins[i].slice(0, 1).toLowerCase() + plugins[i].slice(1, plugins[i].length);
+            if (!fs.existsSync(pluginPath)) continue;
 
+            const moduleClass = (await import(pluginPath)).default;
+            if (!moduleClass) continue;
 
-                        if (this.plugins[pluginName].commands) {
-                            for (const command of this.plugins[pluginName].commands) {
-                                this.commands.set(command.data.name, command);
-                            }
-                        }
-                    }
-                }
-            }
+            await this.loadPlugin(pluginName, moduleClass)
         }
     }
+
+    async loadPlugin(pluginName: string, moduleClass: any) {
+        if ('preloadData' in moduleClass) {
+            const preloadData = await moduleClass.preloadData(this);
+            this.plugins[pluginName] = new moduleClass(this, preloadData);
+        } else {
+            this.plugins[pluginName] = new moduleClass(this);
+        }
+        if (!this.plugins[pluginName].commands) return;
+
+        this.registerPluginCommandsLocaly(this.plugins[pluginName].commands)
+    }
+
+    registerPluginCommandsLocaly(commands: CommandDataObject[]) {
+        for (const command of commands) {
+            this.commands.set(command.data.name, command);
+        }
+    }
+
 
     initInstace() {
         this.instance = new Client({
@@ -186,92 +191,34 @@ export class DiscordBot extends EventDispatcher {
     }
 
     initEvents() {
+        this.initCoreEvents();
+        this.initInteractionHandler();
+    }
+
+    initCoreEvents() {
 
         for (let i = 0; i < eventNames.length; i++) {
             this.instance?.on(eventNames[i], (...args: any[]) => { this.dispatchEvent(`event-${eventNames[i]}`, new Array(...args)) });
         }
         this.instance?.once('ready', async () => {
-            if (!this.instance || !this.instance.user) return;
-
-            console.log(`bot wurde eingeloggt als ${this.instance.user.tag}`);
-
-            this.instance.user.setActivity({
-                name: this.settings.labelActivity,
-                type: ActivityType.Playing
-            });
-
-            const rest = new REST().setToken(this.token);
-
-            const commands: SlashCommandBuilder[] = [];
-
-            this.commands.forEach(cmd => {
-                commands.push(cmd.data);
-            });
-
-            try {
-                await rest.put(Routes.applicationCommands(this.instance.user.id), { body: commands }).catch(err => console.error(err));
-
-                console.log('alle befehle registriert');
-            } catch (err) {
-                console.log(err);
-            }
-            this.dispatchEvent(`event-ready`, new Array(this))
+            await this.registerInstanceAttributes();
         });
+    }
 
+    initInteractionHandler() {
         this.instance?.on('interactionCreate', async (interaction: Interaction) => {
             try {
-                if (interaction.channel && interaction.channel.type === 1) {
+                if (!interaction.guild) {
                     if ('reply' in interaction) interaction.reply('Das geht nur auf Servern.');
                     return;
                 }
 
                 if (interaction.isAutocomplete()) {
-                    const command = this.commands.get(interaction.commandName);
-
-                    if (!command) {
-                        console.error(`No command matching ${interaction.commandName} was found.`);
-                        return;
-                    }
-
-                    if (command.autocomplete) command.autocomplete(this, interaction);
-
-                    return;
-                }
-
-                if (interaction.isChatInputCommand()) {
-                    const command = this.commands.get(interaction.commandName);
-
-                    if (!command) {
-                        console.error(`No command matching ${interaction.commandName} was found.`);
-                        return;
-                    }
-
-                    command.execute(this, interaction);
-
-                    return;
-                }
-
-                let dirName;
-                if (interaction.type === InteractionType.ModalSubmit) {
-                    dirName = 'modal';
-                } else if (interaction.isButton()) {
-                    dirName = 'buttons';
-                } else if (interaction.isStringSelectMenu()) {
-                    dirName = 'selects';
-                } else if (interaction.isUserContextMenuCommand()) {
-                    dirName = 'userContext';
-                }
-                else {
-                    throw new Error('kein interaction type greift.')
-                }
-                const options = ('customId' in interaction) ? interaction.customId.split('-') : [];
-                const name = options.splice(0, 1);
-                const path = `../${dirName}/${name}.js`;
-                if (fs.existsSync(`${homePath}/${path}`)) {
-                    const interactionExecuteFunc = await import(path);
-                    interactionExecuteFunc(this, interaction, options);
+                    this.handleInteractionAutocomplete(interaction);
+                } else if (interaction.isChatInputCommand()) {
+                    this.handleInteracitionChatinputCommand(interaction);
                 } else {
-                    this.dispatchEvent(`interaction-${dirName}-${name}`, [this, interaction, options]);
+                    this.handleComponentInteraction(interaction);
                 }
 
             } catch (err) {
@@ -281,6 +228,70 @@ export class DiscordBot extends EventDispatcher {
                 if ('reply' in interaction) interaction.reply({ embeds: [errorEmbed], ephemeral: true })
             }
         });
+    }
 
+    handleInteractionAutocomplete(interaction: AutocompleteInteraction) {
+
+        const command = this.commands.get(interaction.commandName);
+
+        if (!command) {
+            console.error(`No command matching ${interaction.commandName} was found.`);
+            return;
+        }
+
+        if (command.autocomplete) command.autocomplete(this, interaction);
+    }
+
+    handleInteracitionChatinputCommand(interaction: ChatInputCommandInteraction) {
+        const command = this.commands.get(interaction.commandName);
+
+        if (!command) {
+            console.error(`No command matching ${interaction.commandName} was found.`);
+            return;
+        }
+
+        command.execute(this, interaction);
+    }
+
+    handleComponentInteraction(interaction: Interaction) {
+        let interactionType;
+        if (interaction.type === InteractionType.ModalSubmit) {
+            interactionType = 'modal';
+        } else if (interaction.isButton()) {
+            interactionType = 'buttons';
+        } else if (interaction.isStringSelectMenu()) {
+            interactionType = 'selects';
+        } else if (interaction.isUserContextMenuCommand()) {
+            interactionType = 'userContext';
+        }
+        else {
+            throw new Error('kein interaction type greift.')
+        }
+        const options = ('customId' in interaction) ? interaction.customId.split('-') : [];
+        const name = options.splice(0, 1);
+        this.dispatchEvent(`interaction-${interactionType}-${name}`, [this, interaction, options]);
+    }
+
+    async registerInstanceAttributes() {
+        if (!this.instance || !this.instance.user) return;
+
+        this.instance.user.setActivity({
+            name: this.settings.labelActivity,
+            type: ActivityType.Playing
+        });
+
+        const rest = new REST().setToken(this.token);
+        const commands: SlashCommandBuilder[] = [];
+
+        this.commands.forEach(cmd => {
+            commands.push(cmd.data);
+        });
+
+        try {
+            await rest.put(Routes.applicationCommands(this.instance.user.id), { body: commands }).catch(err => console.error(err));
+        } catch (err) {
+            console.error(err);
+        }
+        this.dispatchEvent(`event-ready`, new Array(this))
     }
 }
